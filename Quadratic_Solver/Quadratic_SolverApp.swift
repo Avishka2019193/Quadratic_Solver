e58b0df1a504b4864560306afd17f0840b806ba6
Avishka2019193 <avishka.2019193@iit.ac.lk>
2023-03-02 01:03:41 +0800

    Initial Commit

---
+++ Quadratic_Solver/Quadratic_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quadratic_SolverApp.swift
+//  Quadratic_Solver
+//
+//  Created by user231229 on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Quadratic_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
